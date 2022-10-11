cfb75feaa672ea6b1cf2a77a189307d728204384
guskova01 <nadegdagus70801@gmail.com>
2022-10-11 10:10:43 +0300

    search detail

---
+++ sky/sky/sky.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+public static class Search
+{
+    public static bool find<T>(this T[] motor, T detail)
+    {
+        return motor.Contains(detail);
+    }
+}
+
+public class Ex
+{
+    public static void Main()
+    {
+        int[] motor = { 1, 2, 3, 4, 5 };
+        int detail = 6;
+
+        bool isThere = motor.find(detail);
+        if (isThere)
+        {
+            Console.WriteLine("Элемент найден ");
+        }
+        else
+        {
+            Console.WriteLine("Элемент не найден в массиве");
+        }
+    }
+}
