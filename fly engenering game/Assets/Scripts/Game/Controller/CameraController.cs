7c083468784f1222f7616022af1b972842cc62a6
VeronikaFedorenko <ronyaufim@icloud.com>
2022-11-27 22:14:44 +0300

    create gear and port controllers

---
+++ fly engenering game/Assets/Scripts/Game/Controller/CameraController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Game
+{
+    public class CameraController : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+            Camera.main.gameObject.SetActive(false);
+            Camera.main.gameObject.SetActive(true);
+        }     
+
+    }
+}
---
+++ fly engenering game/Assets/Scripts/Game/Events/InputEvents.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Game
+{
+    public class InputEvents 
+    {
+        public event Action<Vector2> OnUserInput; 
+        
+        public void UserInput(Vector2 vector2)
+        {
+            OnUserInput?.Invoke(vector2);
+        }
+    }
+
+    
+}
