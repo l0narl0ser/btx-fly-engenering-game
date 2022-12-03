e8511cf0764bd47dd5ff9ff1b724b4066cf7fd9f
VeronikaFedorenko <ronyaufim@icloud.com>
2022-12-03 23:18:45 +0300

    add ability and remove gear

--- fly engenering game/Assets/Scripts/Game/Controller/BuildMechanicsContoller.cs
+++ fly engenering game/Assets/Scripts/Game/Controller/BuildMechanicsContoller.cs
@@ -2,16 +2,19 @@
 using Assets.Scripts.Game.Controller;
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Assets.Scripts.Game
 {
-    public class BuildMechanicsContoller : MonoBehaviour
+    public class BuildMechanicsContoller : MonoBehaviour 
     {
         private MessageSystem _messageSystem;
         private BuildStateMechanics _currentState;
-
         private GearController _currentGearController;
+
+        private Dictionary<GearController, PortController> _accordancePortToGear = new Dictionary<GearController, PortController>();
+
 
         private void Awake()
         {
@@ -40,6 +43,7 @@ namespace Assets.Scripts.Game
             PortController selectedPortController = gameObject.GetComponent<PortController>();
             GearController selectedGearController = gameObject.GetComponent<GearController>();
             BuildStateMechanics newState = BuildStateMechanics.NONE;
+
             if(selectedPortController != null)
             {
                 newState = BuildStateMechanics.SELECT_PORT;
@@ -47,6 +51,17 @@ namespace Assets.Scripts.Game
             else
             {
                 newState = BuildStateMechanics.SELECT_GEAR;
+                _currentGearController = selectedGearController;
+            }
+
+            if (newState == BuildStateMechanics.SELECT_GEAR && selectedGearController.Inserted)
+            {
+                selectedGearController.ReturnToInitPostion();
+                PortController removingPort = _accordancePortToGear[selectedGearController];
+                removingPort.Occupied = false;
+                _accordancePortToGear.Remove(selectedGearController);
+                _currentState = BuildStateMechanics.NONE;
+                return;
             }
 
             if(_currentState == BuildStateMechanics.SELECT_GEAR && newState == BuildStateMechanics.SELECT_PORT)
@@ -54,24 +69,21 @@ namespace Assets.Scripts.Game
                 //TODO:Перенести шестеренку на порт
                 _currentState = BuildStateMechanics.NONE;
                 _currentGearController.gameObject.transform.position = selectedPortController.transform.position;
+                _currentGearController.Inserted = true;
+                selectedPortController.Occupied = true;
+                _accordancePortToGear.Add(_currentGearController, selectedPortController);
                 return;
             }
 
             if(_currentState == BuildStateMechanics.NONE && newState == BuildStateMechanics.SELECT_GEAR)
             {
                 _currentState = newState;
-                _currentGearController = selectedGearController;
                 //TODO:: СДелать выбранную шестренку больше или изменить цвет цветом
                 return;
             }
 
-            if (_currentState == BuildStateMechanics.NONE && newState == BuildStateMechanics.SELECT_PORT)
-            {
-                _currentState = newState;
-                //TODO:: Cделать вибрацию или увелечение чего-то
-                return;
-            }
             //вернуть шестеренку назад
+
         }
     }
 }
