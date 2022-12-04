69403825a7b1efe5d652852c68f68a5b4bfde9f4
VeronikaFedorenko <ronyaufim@icloud.com>
2022-12-04 22:13:35 +0300

    create time service

---
+++ fly engenering game/Assets/Scripts/Game/Service/TimeService.cs
@@ -0,0 +1,36 @@
+﻿using Assets.Scripts.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.Game.Service
+{
+    public class TimeService : IDisposable
+    {
+        private MessageSystem _messageSystem;
+        private float _timeLevelStart;
+
+        public TimeService()
+        {
+            _messageSystem.LevelEvents.OnLevelStated += OnLevelStarted;
+        }
+
+        public void Dispose()
+        {
+            _messageSystem.LevelEvents.OnLevelStated -= OnLevelStarted;
+        }
+
+        private void OnLevelStarted()
+        {
+            //Взять время
+        }
+
+        public float GetLevelTime()
+        {
+            return Time.realtimeSinceStartup - _timeLevelStart;
+        }
+    }
+}
